94cb1a291343fb996151a6e67bc82e94e61422eb
Gizot <tastangizem@gmail.com>
2023-07-23 22:10:04 +0300

    Created Grade class which is an Enum class

---
+++ hw3/src/util/Grade.java
@@ -0,0 +1,27 @@
+package util;
+
+public enum Grade {
+    A("A", 4), B("B", 3), C("C", 2), D("D", 1), F("F", 0);
+
+    private String stringValue;
+    private int numericValue;
+
+    private Grade(String stringValue, int numericValue) {
+        this.stringValue = stringValue;
+        this.numericValue = numericValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public int getNumericValue() {
+        return numericValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Grade " + stringValue + " corresponds to numeric grade " + numericValue;
+    }
+}
+
